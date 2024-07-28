35423af1b544266373b5b16f40830493e4f3df55
Fritz Ammon <fritz.ammon@slickdeals.net>
2024-07-27 20:52:11 -0700

    Add fresh new Xcode project

---
+++ WhatsTheDealio/WhatsTheDealioApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsTheDealioApp.swift
+//  WhatsTheDealio
+//
+//  Created by Fritz Ammon on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsTheDealioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

899f2d6115b0761e212d03370dbe17fca06a041e
Fritz Ammon <fritz.ammon@slickdeals.net>
2024-07-28 02:39:17 -0700

    Add CachedAsyncImage

---
+++ WhatsTheDealio/Views/Custom/CachedAsyncImage.swift
@@ -0,0 +1,38 @@
+//
+//  CachedAsyncImage.swift
+//  WhatsTheDealio
+//
+//  Created by Fritz Ammon on 7/28/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct CachedAsyncImage<Content: View, Placeholder: View>: View {
+    let url: URL?
+    let content: (Image) -> Content
+    let placeholder: Placeholder
+    
+    @State private var image: UIImage?
+    
+    init(url: URL?, @ViewBuilder content: @escaping (Image) -> Content, @ViewBuilder placeholder: () -> Placeholder) {
+        self.url = url
+        self.content = content
+        self.placeholder = placeholder()
+    }
+    
+    var body: some View {
+        if let image {
+            content(Image(uiImage: image))
+        } else {
+            placeholder
+                .task {
+                    guard let url,
+                          let (data, _) = try? await URLSession.shared.data(from: url)
+                    else { return }
+                    
+                    image = UIImage(data: data)
+                }
+        }
+    }
+}

6c02dec6ff6679bd60c2174e9a9fe973b45bb20a
Fritz Ammon <fritz.ammon@slickdeals.net>
2024-07-28 21:00:08 -0700

    Fixed data race potential caused by await re-entrancy

--- WhatsTheDealio/Utilities/Helpers/DeeplinkManager/DeeplinkManager.swift
+++ WhatsTheDealio/Utilities/Helpers/DeeplinkManager/DeeplinkManager.swift
@@ -14,7 +14,7 @@ class DeeplinkManager {
     private let regexMatchers: [DeeplinkRegexMatcher]
     
     private(set) var deeplink: Deeplink?
-
+    
     private let deeplinkConsumer = Consumer()
     
     init(appScheme: String, hostName: String, regexMatchers: [DeeplinkRegexMatcher]) {
@@ -22,53 +22,66 @@ class DeeplinkManager {
         self.hostName = hostName
         self.regexMatchers = regexMatchers
     }
-
-    func consumeDeeplink(_ consumption: (Deeplink) async -> Bool) async {
+    
+    func consumeDeeplink(_ consumption: @escaping (Deeplink) async -> Bool) async {
         await deeplinkConsumer.consumeDeeplink(&deeplink, consumption: consumption)
     }
-
+    
     func parseURL(_ url: URL) async throws {
         let modifiedURL = url.scheme == appScheme ? url.movingHostToPath() : url
-
+        
         if modifiedURL.scheme?.hasPrefix("http") == true {
             self.deeplink = try await processHTTPURL(modifiedURL)
         } else {
             self.deeplink = matchDeeplink(in: modifiedURL)
         }
     }
-
+    
     private func processHTTPURL(_ url: URL) async throws -> Deeplink? {
         let deepLink = matchDeeplink(in: url)
-
+        
         guard deepLink == nil else { return deepLink }
-
+        
         let redirectedURL = try await url.checkForRedirect(hostName: hostName)
-
+        
         return try await processHTTPURL(redirectedURL)
     }
-
+    
     private func matchDeeplink(in url: URL) -> Deeplink? {
         let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
-
+        
         guard let path = urlComponents?.path else { return nil }
-
+        
         for regexMatcher in regexMatchers {
             guard let deeplink = regexMatcher.matchDeeplink(path) else { continue }
             return deeplink
         }
-
+        
         return nil
     }
-
+    
     actor Consumer {
-        func consumeDeeplink(_ deeplink: inout Deeplink?, consumption: (Deeplink) async -> Bool) async {
-            guard let validDeeplink = deeplink else { return }
-
-            let consumed = await consumption(validDeeplink)
-
-            guard consumed else { return }
-
-            deeplink = nil
+        var consumptionTasks: [Task<Bool, Never>] = []
+        
+        func consumeDeeplink(_ deepLink: inout Deeplink?, consumption: @escaping (Deeplink) async -> Bool) async {
+            for task in consumptionTasks {
+                guard await task.value else { continue }
+                return
+            }
+            
+            guard let validDeepLink = deepLink else { return }
+            
+            let newTask = Task {
+                await consumption(validDeepLink)
+            }
+            
+            consumptionTasks.append(newTask)
+            
+            defer { consumptionTasks.removeAll() }
+            
+            guard await newTask.value else { return }
+            
+            deepLink = nil
         }
     }
 }
